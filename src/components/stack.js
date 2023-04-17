e803c6b3f76f23b8c5472c4949237be2bf15a8cd
kunal passan <Kunalpassan30@gmail.com>
2023-04-17 20:05:09 +0530

    brand stack updated

--- src/components/stack.js
+++ src/components/stack.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import stack from '../assets/stack.png'
 import '../styles/stack.css'
 import custom from '../assets/custom.png'
 import arrow from '../assets/arrow.png'
 import task from '../assets/task.png'
 // import tick from '../assets/tick.png'
+import brand1 from '../assets/brand1.png'
+import brand2 from '../assets/brand2.png'
+import brand3 from '../assets/brand3.png'
+import brand4 from '../assets/brand4.png'
+import brand5 from '../assets/brand5.png'
+import brand6 from '../assets/brand6.png'
 
 function Stack() {
   return (
     <>
-        <div className='stack-cubex'>
-            <img src={stack} alt='tech-stack'/>
+        <div className='stacks-cubex'>
+            <img src={brand1} alt='brand1'/>
+            <img src={brand2} alt='brand2'/>
+            <img src={brand3} alt='brand3'/>
+            <img src={brand4} alt='brand4'/>
+            <img src={brand5} alt='brand5'/>
+            <img src={brand6} alt='brand6'/>
         </div>
         <div className='custom-cubex'>
             <img src={custom} alt='custom-stack'/>
